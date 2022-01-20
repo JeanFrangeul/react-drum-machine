6ebbe3e5d8528b9df76d6873715910a85cef805a
JeanFrangeul <frangeuljean@gmail.com>
2022-01-20 20:23:55 +0100

    rebuild sans readme

--- src/componnents/About/index.js
+++ src/componnents/About/index.js
@@ -7,9 +7,9 @@ const About = () => {
             <h2>Instructions</h2>
 
             <section>
-                <p>Jouez les sons en :</p>
-                <p>-cliquant sur les différents bouttons</p>
-                <p>-tapant au clavier "A Z E R"</p>
+                <p>Jouez les sons :</p>
+                <p>-en cliquant sur les différents boutons</p>
+                <p>-en tapant au clavier "A Z E R"</p>
                 <div>
                     Chargez de nouveaux sons en cliquant sur "<span>♫</span>"
                 </div>
